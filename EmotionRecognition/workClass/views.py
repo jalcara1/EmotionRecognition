5ca336e25cb0e9d63283feda7fae92ee220fd6ee
Juan Pablo Alcaraz Florez <jalcara1@eafit.edu.co>
2018-05-21 00:08:09 -0500

    Some Problems

--- EmotionRecognition/workClass/views.py
+++ EmotionRecognition/workClass/views.py
@@ -3,12 +3,6 @@ from .models import Curso, Docente, Estudiante, Tema, Video
 from .forms import videoForm
 import json
 
-import boto3
-import subprocess
-from PIL import Image
-import os
-
-emociones = ["HAPPY", "ANGRY", "SURPRICED", "SAD", "CALM", "DISGUSTED", "CONFUSE"]
 try:
     todos_videos = Video.objects.all()
     reprod_video = todos_videos[0]
@@ -109,7 +103,7 @@ def formularios_2(request):
             post2.video = str("Emoji" + nameVideo)
             post2.modificado = True
             post2.contenido = True
-            post2.emocion = str(classifier(nameVideo))
+            #post2.emocion = str(classifier(nameVideo))
             post2.save()
 
     else:
@@ -171,91 +165,3 @@ def estadisticas_video(request, video_id):
     video = Video.objects.get(pk=video_id)
     return render(request, 'workClass/estadisticas.html', {'todos_videos': todos_videos,
                                                            'video': video})
-
-
-def classifier(nameVideo):
-    nameVideo = nameVideo[0:-4]
-    s3 = boto3.resource('s3')
-    prefix = "media/"
-    Emoji = prefix + "imagenes/Emoji/"
-    bucket = 'emotion.recognition.db'
-    client = boto3.client('rekognition', 'us-east-1')
-    confianza_final = -1.0
-    emotions = []
-    all_emotions = ["HAPPY", "ANGRY", "SURPRISED", "SAD", "CALM", "DISGUSTED", "CONFUSED"]
-    emoticons = {
-        "HAPPY": Emoji + 'happy.png',
-        "ANGRY": Emoji + 'angry.png',
-        "SURPRISED": Emoji + 'surprised.png',
-        "SAD": Emoji + 'sad.png',
-        "CALM": Emoji + 'calm.png',
-        "DISGUSTED": Emoji + 'disgusted.png',
-        "CONFUSED": Emoji + 'confused.png',
-        "UNKNOWN": Emoji + 'unknown.png'
-    }
-    os.makedirs(prefix + "imagenes/" + nameVideo, exist_ok=True)
-    os.makedirs(prefix + "imagenes/" + nameVideo + "Emoji", exist_ok=True)
-    videoFrames = "ffmpeg -y -i " + prefix + nameVideo + ".mp4 -r 1 " + prefix + "imagenes/" + nameVideo + "/output_%05d.png"
-    subprocess.call(['bash', '-c', videoFrames])
-
-    for dirpath, dirs, files in os.walk(prefix + "imagenes/" + nameVideo):
-        for fileName in files:
-            fileNameEmoji = fileName
-            print("------------------------------------------->>>>>" + fileName)
-            fileName = prefix + "imagenes/" + nameVideo + "/" + fileName
-            print("\n ---------------------------->>>>>" + fileName)
-            s3.meta.client.upload_file(fileName, bucket, fileName)
-            response = client.detect_faces(Image={'S3Object': {'Bucket': bucket, 'Name': fileName}}, Attributes=['ALL'])
-            print("------------------------------------------->>>>>" + "1")
-            source_img = s3.Object(bucket, fileName).get()
-            im = Image.open(source_img.get('Body'))
-
-            for face in response.get('FaceDetails'):
-                w = face.get('BoundingBox').get('Width')
-                h = face.get('BoundingBox').get('Height')
-                l = face.get('BoundingBox').get('Left')
-                t = face.get('BoundingBox').get('Top')
-                x1 = im.size[0] * l
-                y1 = im.size[1] * t
-                x2 = x1 + im.size[0] * w
-                y2 = y1 + im.size[1] * h
-                fimemo = face.get('Emotions')[0].get('Type')
-                imemo = Image.open(emoticons.get(fimemo))
-                a = int(x2 - x1)
-                b = int(y2 - y1)
-                imemo = imemo.resize((a, b))
-                im.paste(imemo, (int(x1), int(y1)), imemo)
-            print("------------------------------------------->>>>>" + prefix + "imagenes/" + nameVideo + "Emoji/" + fileNameEmoji, "PNG")
-            im.save(prefix + "imagenes/" + nameVideo + "Emoji/" + fileNameEmoji, "PNG")
-            for faceDetail in response['FaceDetails']:
-                for emotion in faceDetail['Emotions']:
-                    confianza = float(str(emotion["Confidence"]))
-                    emocion = str(emotion["Type"])
-                    if confianza > confianza_final:
-                        emocion_final = emocion
-                        confianza_final = confianza
-                emotions = emotions + [emocion_final]
-                emotions = emotions + [emocion_final]
-                confianza_final = -1.0
-
-    data_emociones = {"HAPPY": float(str(emotions.count("HAPPY"))),
-                      "ANGRY": float(str(emotions.count("ANGRY"))),
-                      "SURPRISED": float(str(emotions.count("SURPRISED"))),
-                      "SAD": float(str(emotions.count("SAD"))),
-                      "CALM": float(str(emotions.count("CALM"))),
-                      "DISGUSTED": float(str(emotions.count("DISGUSTED"))),
-                      "CONFUSED": float(str(emotions.count("CONFUSED")))}
-
-    emotions_json = json.dumps(data_emociones)
-    with open(prefix + nameVideo + '.json', 'w') as file:
-        json.dump(emotions_json, file, ensure_ascii=False)
-
-    # emptyBucket = "aws s3 rm s3://emotion.recognition.db --recursive"
-    # subprocess.call(['bash','-c', emptyBucket])
-    newVideo = "ffmpeg -r 1 -i " + prefix + "imagenes/" + nameVideo + "Emoji/output_%05d.png -framerate 1 -strict -2 -pix_fmt yuv420p -c:v libx264 -c:a aac -y " + prefix + "Emoji" + nameVideo + ".mp4"
-    subprocess.call(['bash', '-c', newVideo])
-
-    return nameVideo + '.json'
-
-
-
